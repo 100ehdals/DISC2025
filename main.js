19c26a9c3a38e550c4df9b9f50339adc6631e38c
100ehdals <100ehdals@naver.com>
2025-08-08 00:18:26 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,63 @@
-/* Get elements for keyboard buttons */
+/* Get elements in HTML */
 const keyboardBtn = document.querySelector("button");
-
-/* Get elements for timer */
 const progressBar = document.querySelector(".progress-inner");
-
-/* Get elements for wpm display */
+const inputField = document.querySelector(".overlay-input");
 const dispState = document.querySelector(".result");
-
-/* Get elements for the input/request field */
+const resultBtn = document.querySelector(".show-result");
 const reqStr = document.querySelector(".req-str");
 const inputStr = document.querySelector(".overlay-input");
 
 
+/* Define variables */
 var isTyping = false;  // Check if user started typing
+const countingTime = 60;  // timeout in seconds
 
 /* Typearea */
 
 
 /* WPM calculation */
+function calculateWpm() {
+    let segmentInterval = 100;
+    let segmentInputCount = 0;
+    let arrayLen = countingTime * (1000 / segmentInterval);
+    let segmentList = new Array(arrayLen + (1000 / segmentInterval)).fill(0);
+    let segmentNo = 0;  // nth segment
+    let currentCpm = 0;
+
+    inputField.addEventListener("input", () => {
+        segmentInputCount++;
+
+        // count characters in the inputfield
+        const typedText = inputField.value.trim();
+        const inputLength = typedText.length;
+    });
+
+    // update cpm every 100 msec
+    var cpsCalc = setInterval(() => {
+        segmentList[segmentNo + (1000/segmentInterval) - 1] = segmentInputCount;
+
+        // calculate cps
+        for (let i=segmentNo; i<segmentNo+(1000/segmentInterval); i++) {
+            currentCpm += segmentList[i];
+        }
+
+        console.log(segmentList);
+
+        dispState.innerHTML = currentCpm * 12 + "wpm";
+        segmentNo++;
+
+        segmentInputCount = 0;
+        currentCpm = 0;
+
+        // if timeout then stop calculating
+        if (isTyping == false) clearInterval(cpsCalc);
+    }, segmentInterval);
+}
+
 
 
 /* Timer */ 
 function activateTimer() {
-    console.log("Timer activating");
-    const countingTime = 30;  // Counting time in seconds
     let interval = 10;  // update progress bar every 10 msec
     let totalTicks = countingTime * 1000 / interval;
     let tickCount = totalTicks;
@@ -35,49 +69,59 @@ function activateTimer() {
 
         if (tickCount > 0) {
             progressBar.style.width = 100 - progressWidth + "%";
-            checkColors(progressWidth);
+            setProgressBarColor(progressWidth);
         }
         else {
             clearInterval(countdown);
             progressBar.style.width = "100%";
             progressBar.style.background = "gray";
             inputStr.disabled = true;
-            console.log("Timer end / Press space to restart");
-            dispState.innerHTML = "Press TAB to restart";
+            resultBtn.style.display = 'block';
             isTyping = false;
         }
     }, interval);
 }
 
-const checkColors = (width) => {
-    if (width > 50) progressBar.style.background = "green";
-    else if (width > 30) progressBar.style.background = "yellow";
-    else if (width > 10) progressBar.style.background = "orange";
-    else progressBar.style.background = "red";
+const setProgressBarColor = (width) => {
+    let deg = (width - 90) * 1.3;
+    progressBar.style.filter = `hue-rotate(${deg}deg)`;
 }
 
 
+/* Result */
+
+
 /* Keyboard Animation */
-// keydown 동안 버튼 배경 바꾸기 anim
 window.addEventListener('keydown', e => {
     const pressedKey = document.getElementById(e.key);
-    console.log(e.key);
 
     if (!isTyping && e.key === 'Tab') {
         console.log("Start");
-        reqStr.innerHTML = "They traveled night and day through a dangerous snowstorm. While temperatures dropped to negative 62 degrees, several dog died from exhaustion. But after five days of constant traveling, they finally arrived. And the town was saved by the dedication of the dogs.";
+        reqStr.innerHTML =
+            "A person who thinks all the time has nothing to think except thoughts. \
+            So, he loses touch with reality and live in the world of illusions. \
+            By thoughts, I mean specifically, chatter in the skull, \
+            perpetual and compulsive repetition of words, of reckoning and calculating.";
         isTyping = true;
         inputStr.disabled = false;
         activateTimer();
+        calculateWpm();
     }
     
     if (pressedKey) pressedKey.classList.add("pressed");
 });
 
-// keyup하면 원래의 버튼 배경으로 유지
 window.addEventListener('keyup', e => {
     const pressedKey = document.getElementById(e.key);
     if (pressedKey) pressedKey.classList.remove("pressed");
 });
 
 
+/* Result button animation */
+resultBtn.addEventListener("mousedown", () => {
+    resultBtn.classList.add("clicked")
+});
+
+resultBtn.addEventListener("mouseup", () => {
+    resultBtn.classList.remove("clicked");
+})
